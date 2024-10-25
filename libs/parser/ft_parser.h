d30adc9d7562c82203a7dc99fff05b2c86500085
JoseLievano <lievanosan@gmail.com>
2024-10-25 13:08:10 +0200

    Add parser enums

--- libs/parser/ft_parser.h
+++ libs/parser/ft_parser.h
@@ -6,7 +6,7 @@
 /*   By: jlievano <jlievano@student.42luxembourg.>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 11:59:29 by jlievano          #+#    #+#             */
-/*   Updated: 2024/10/25 12:50:21 by jlievano         ###   ########.fr       */
+/*   Updated: 2024/10/25 13:07:36 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,27 @@ typedef enum cmd_tab_type
 {
 	PIPE_SEQ,
 	COMMAND
-} t_cmdtype;
+} t_cmd_type;
+
+typedef enum redirection_type
+{
+	REDIR_INPUT,
+	REDIR_OUTPUT,
+	REDIR_APPEND,
+	REDIR_HEREDOC
+} t_redir_type;
+
+typedef enum argument_type
+{
+	ARGUMENT,
+	FLAG,
+	STRING_DOUBLE,
+	STRING_SINGLE
+} t_arg_type;
 
 typedef struct command_table
 {
-	t_cmdtype	type;
+	t_cmd_type	type;
 	void		*content;
 } t_cmdt;
 
@@ -30,8 +46,16 @@ typedef struct pipe_sequence
 {
 	char	*name;
 	int		cmd_count;
-	t_dll	*commands;
+	t_dll	*cmds;
 } t_pipes;
+
+typedef struct redirection_obj
+{
+	t_redir_type	type;
+	char			*delimiter;
+	char			*file_path;
+	int				fd;
+};
 
 typedef struct command_obj
 {
