f48ca2f4f08584450642b07272f90a704ee65783
JoseLievano <lievanosan@gmail.com>
2024-10-27 20:22:39 +0100

    Add token types enums

--- libs/parser/ft_token_type_check.c
+++ libs/parser/ft_token_type_check.c
@@ -1,34 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_reader.h                                        :+:      :+:    :+:   */
+/*   ft_token_type_check.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlievano <jlievano@student.42luxembourg.>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/11 01:28:59 by jlievano          #+#    #+#             */
-/*   Updated: 2024/10/27 20:21:30 by jlievano         ###   ########.fr       */
+/*   Created: 2024/10/27 20:04:13 by jlievano          #+#    #+#             */
+/*   Updated: 2024/10/27 20:04:42 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_READER_H
-# define FT_READER_H
-# include "../../inc/minishell.h"
+#include "ft_parser.h"
 
-typedef enum  token_type
-{
-	TOKEN_END_INPUT,
-	TOKEN_PIPE,
-	TOKEN_REDIRECTION_APPEND,
-	TOKEN_TOKEN_HEREDOC,
-	TOKEN_REDIRECTION_IN,
-	TOKEN_REDIRECTION_OUT,
-	TOKEN_FLAG,
-	TOKEN_STRING_DOUBLE,
-	TOKEN_STRING_SINGLE,
-	TOKEN_COMMAND,
-	TOKEN_ARGUMENT
-};
-
-char	*ft_reader(void);
-
-#endif
