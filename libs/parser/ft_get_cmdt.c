41e354d75a90ce5b8a34d2984e5ea6c2e683d10e
Jose Lievano <jlievano@c3r7s2.42luxembourg.lu>
2024-11-04 19:31:30 +0100

    Start setting cmdt type

--- libs/parser/ft_get_cmdt.c
+++ libs/parser/ft_get_cmdt.c
@@ -12,3 +12,13 @@
 
 #include "ft_parser.h"
 
+t_cmdt  *ft_get_cmdt(t_dll *token_list)
+{
+   t_cmdt   *cmdt;
+
+   cmdt = (t_cmdt *)malloc(sizeof(cmdt));
+   ft_set_cmdt_type(cmdt, token_list);
+   if (!cmdt->content && t_dll_size(token_list) == 0)
+      return NULL;
+   return (cmdt);
+}
--- libs/parser/ft_parser.c
+++ libs/parser/ft_parser.c
@@ -24,5 +24,8 @@ void	ft_parser(void)
 	else
 		printf("Invalid syntax");
 	//print_cmd_table(get_test(3));
+	cmdt = ft_get_cmdt(token_list);
+	if (cmdt)
+		printf("\nCmdt setted");
 }
 
