fc58d2f5046e1edc1b035564447fd6b352b436c6
JoseLievano <lievanosan@gmail.com>
2024-11-13 12:12:22 +0100

    Check cmdt in executor

--- libs/executor/ft_executor.c
+++ libs/executor/ft_executor.c
@@ -6,7 +6,7 @@
 /*   By: jlievano <jlievano@student.42luxembourg.>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 19:43:59 by jlievano          #+#    #+#             */
-/*   Updated: 2024/11/11 22:54:46 by jlievano         ###   ########.fr       */
+/*   Updated: 2024/11/13 12:11:18 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 
 void ft_executor(void)
 {
-
+	t_cmdt *cmdt = ft_parser(read_through_input(ft_reader()));
+	print_cmd_table(cmdt);
+	ft_clean_cmdt(cmdt);
 }
