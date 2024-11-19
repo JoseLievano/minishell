9ce24111475b0438543054fb8e3c810ea8d1c625
JoseLievano <lievanosan@gmail.com>
2024-11-19 23:45:42 +0100

    Make sure that ft_find_env function works in expander

--- libs/expander/ft_expander.c
+++ libs/expander/ft_expander.c
@@ -3,4 +3,6 @@
 void	ft_expander(t_minishell *minishell)
 {
 	printf("\nHello from expander, envs %d", (int)t_dll_size(minishell->envs));
+	t_dll *path_node = ft_find_env("PATH", minishell->envs);
+	printf("\nPath value: %s", ((t_env *)path_node->content)->value);
 }
