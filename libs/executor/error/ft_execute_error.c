d22371244eece2d61770641b2daec6c8490ebc3c
JoseLievano <lievanosan@gmail.com>
2024-11-24 16:00:51 +0100

    Add error and redirections initial functions

--- libs/executor/error/ft_execute_error.c
+++ libs/executor/error/ft_execute_error.c
@@ -1,18 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_input.c                                         :+:      :+:    :+:   */
+/*   ft_execute_error.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlievano <jlievano@student.42luxembourg.l  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 20:01:37 by jlievano          #+#    #+#             */
-/*   Updated: 2024/11/21 20:01:38 by jlievano         ###   ########.fr       */
+/*   Created: 2024/11/24 15:13:28 by jlievano          #+#    #+#             */
+/*   Updated: 2024/11/24 15:13:29 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_executor.h"
 
-void	ft_process_input(t_redir *input)
-{
-	printf("%s", input->file_path);
-}
+
--- libs/executor/redirections/ft_process_redirections.c
+++ libs/executor/redirections/ft_process_redirections.c
@@ -1,18 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_input.c                                         :+:      :+:    :+:   */
+/*   ft_process_redirections.c                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jlievano <jlievano@student.42luxembourg.l  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/21 20:01:37 by jlievano          #+#    #+#             */
-/*   Updated: 2024/11/21 20:01:38 by jlievano         ###   ########.fr       */
+/*   Created: 2024/11/21 20:22:50 by jlievano          #+#    #+#             */
+/*   Updated: 2024/11/21 20:22:51 by jlievano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_executor.h"
 
-void	ft_process_input(t_redir *input)
-{
-	printf("%s", input->file_path);
-}
